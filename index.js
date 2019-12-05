a7d8d4ad5dc94e59f1f526aab2f0b8e88e1c9666
Erik Larsson <erik.mattias.larsson@gmail.com>
2019-12-05 07:38:35 +0100

    Trigger build

--- index.js
+++ index.js
@@ -50,5 +50,3 @@ app.get('/matches', (req, res) => res.json(matches))
 app.get('/schedule', (req, res) => res.json(schedule))
 
 app.listen(port, () => console.log(`Smash ladder BE started - listening on port ${port}`))
-
-//
