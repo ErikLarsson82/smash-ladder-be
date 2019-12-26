1a92f6c1a1a6a4a4da769c28e0f2f367e2f68d69
Erik Larsson <erik.mattias.larsson@gmail.com>
2019-12-26 10:08:51 +0100

    removed old json scripts

--- commit-push.sh
+++
@@ -1,6 +0,0 @@
-git add players.json
-git add matches.json
-git add schedule.json
-git commit -m "Auto-update players, matches and schedule"
-git push origin master
-git push heroku master
--- update.sh
+++
@@ -1,3 +0,0 @@
-curl -o matches.json 'https://hiqombo-ladder-be.herokuapp.com/matches'
-curl -o schedule.json 'https://hiqombo-ladder-be.herokuapp.com/schedule'
-curl -o players.json 'https://hiqombo-ladder-be.herokuapp.com/players'
